91934867ad625b4e82dece99cc68b2ea62595165
flaviobergamini <fhmbergamini@hotmail.com.br>
2025-06-11 18:27:54 -0300

    feat: creating redis module for dependecy injection

---
+++ apps/api/src/modules/redis.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
+import { RedisCacheService } from 'src/external/services/redis-cache-service';
+import { config } from 'src/config';
+import { ListProductController } from 'src/adapters/controllers/list-products-controller';
+import { ListProductsUseCase } from 'src/usecases/list-products/list-products-usecase';
+import { MongoProductRepository } from 'src/external/orm/repositories/mongo-product-repository';
+import { ProductModule } from './product.module';
+
+@Module({
+    imports: [
+        CacheModule.registerAsync({
+        useFactory: () => ({
+            store: redisStore,
+            socket: {
+            host: config.REDIS.HOST,
+            port: config.REDIS.PORT,
+            },
+            username: config.REDIS.USER_NAME,
+            password: config.REDIS.PASSWORD,
+            ttl: 60,
+        }),
+        }),
+        ProductModule,
+    ],
+    controllers: [ListProductController],
+    providers: [
+          {
+          provide: 'ProductRepository',
+          useClass: MongoProductRepository,
+          },
+          {
+          provide: 'CacheService',
+          useClass: RedisCacheService,
+          },
+          {
+          provide: ListProductsUseCase,
+          useClass: ListProductsUseCase,
+          },
+    ],
+    exports: ['CacheService'],
+})
+export class RedisModule {}
