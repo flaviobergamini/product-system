965a066116e1b117aaaadde154a7d26d6d53614c
flaviobergamini <fhmbergamini@hotmail.com.br>
2025-06-12 14:38:30 -0300

    feat: adding web socket in add product use case to emit socket for new product

--- apps/api/src/usecases/add-product/add-product-use-case.ts
+++ apps/api/src/usecases/add-product/add-product-use-case.ts
@@ -1,22 +1,40 @@
-import { ProductRepository } from "../interfaces/repositories/product-repository";
-import { Product } from "src/entities/product";
-import { randomUUID } from "crypto";
-import { Inject, Injectable } from "@nestjs/common";
-import { CacheService } from "../interfaces/services/cache-service";
+import { ProductRepository } from '../interfaces/repositories/product-repository';
+import { Product } from 'src/entities/product';
+import { randomUUID } from 'crypto';
+import { Inject, Injectable } from '@nestjs/common';
+import { CacheService } from '../interfaces/services/cache-service';
+import { SocketService } from '../interfaces/services/socket-service';
 
 @Injectable()
 export class AddProductUseCase {
-    constructor(
-      @Inject('ProductRepository') private readonly productRepository: ProductRepository,
-      @Inject('CacheService') private cache: CacheService,
-    ) {}
+  constructor(
+    @Inject('ProductRepository')
+    private readonly productRepository: ProductRepository,
+    @Inject('CacheService') private readonly cache: CacheService,
+    @Inject('SocketService') private readonly webSocket: SocketService,
+  ) {}
 
-    async execute(name: string, category: string, quantity: number, price: number, description: string): Promise<Product> {
-    const product = new Product(randomUUID(), name, category, quantity, price, description);
+  async execute(
+    name: string,
+    category: string,
+    quantity: number,
+    price: number,
+    description: string,
+  ): Promise<Product> {
+    const product = new Product(
+      randomUUID(),
+      name,
+      category,
+      quantity,
+      price,
+      description,
+    );
     const productDb = await this.productRepository.create(product);
 
     const products = await this.productRepository.findAll();
     await this.cache.set('products', products);
+
+    this.webSocket.broadcastNewProduct(product);
 
     return productDb;
   }
