a239313b7b5031cad835abb87689b00c685f6170
flaviobergamini <fhmbergamini@hotmail.com.br>
2025-06-13 18:44:24 -0300

    feat: creating paginated search, modifying endpoint for paginated list and by category, adding logs and error handling. Adjusting indentation

--- apps/api/src/usecases/add-product/add-product-use-case.ts
+++ apps/api/src/usecases/add-product/add-product-use-case.ts
@@ -1,12 +1,14 @@
 import { ProductRepository } from '../interfaces/repositories/product-repository';
 import { Product } from 'src/entities/product';
 import { randomUUID } from 'crypto';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { CacheService } from '../interfaces/services/cache-service';
 import { SocketService } from '../interfaces/services/socket-service';
 
 @Injectable()
 export class AddProductUseCase {
+  private readonly logger = new Logger(AddProductUseCase.name);
+
   constructor(
     @Inject('ProductRepository')
     private readonly productRepository: ProductRepository,
@@ -21,21 +23,36 @@ export class AddProductUseCase {
     price: number,
     description: string,
   ): Promise<Product> {
-    const product = new Product(
-      randomUUID(),
-      name,
-      category,
-      quantity,
-      price,
-      description,
-    );
-    const productDb = await this.productRepository.create(product);
+    try {
+      this.logger.log('Iniciando criação de produto');
 
-    const products = await this.productRepository.findAll();
-    await this.cache.set('products', products);
+      const product = new Product(
+        randomUUID(),
+        name,
+        category,
+        quantity,
+        price,
+        description,
+      );
 
-    this.webSocket.broadcastNewProduct(product);
+      const productDb = await this.productRepository.create(product);
 
-    return productDb;
+      this.logger.log(`Produto criado com ID: ${productDb.id}`);
+      this.logger.log('Atualizando cache do Redis');
+
+      const products = await this.productRepository.findAll();
+      await this.cache.set('products', products);
+
+      this.logger.log('Notificando adição de novo produto por WebSocket');
+
+      this.webSocket.broadcastNewProduct(product);
+
+      this.logger.log('Produto adicionado com sucesso');
+
+      return productDb;
+    } catch (error) {
+      this.logger.error('Erro ao criar produto', error);
+      throw error;
+    }
   }
 }
