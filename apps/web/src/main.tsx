47e10e83245b44eb5916d25e520980801b3715b2
flaviobergamini <fhmbergamini@hotmail.com.br>
2025-06-14 19:52:47 -0300

    feat: adding components in main.tsx file

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import ProductListPage from './presentation/pages/product-list/product-list'
+import HeaderComponent from './presentation/components/header'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeaderComponent />
+    <ProductListPage />
   </StrictMode>,
 )
